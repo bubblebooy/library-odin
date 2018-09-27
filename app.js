169ec9bdd011221979413e3050e69949ac5d1024
Omid Panahi <pnhomid@gmail.com>
2018-09-27 21:50:25 +0330

    some changes

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const inputTitle = document.getElementById('btitle');
 const inputAuthor = document.getElementById('bauthor');
 const inputNumber = document.getElementById('bnumber');
 const inputRead = document.getElementById('bread');
-
+const bookslist = document.getElementById('bookslist');
 
 function Book(title, author, pages, read) {
   this.title = title;
@@ -22,16 +22,40 @@ form.addEventListener('submit', function(e){
   let author = inputAuthor.value;
   let pages = inputNumber.value;
   let read = inputRead.checked;
-  let book = new Book(title, author, pages, read);
-
-  addBook(book);
+  let newBook = new Book(title, author, pages, read);
+  addBook(newBook);
+  viewAddBook(newBook);
+  inputTitle.value = "";
+  inputAuthor.value = "";
+  inputNumber.value = "";
+  inputRead.checked = false;
   e.preventDefault();
+})
+
+
+bookslist.addEventListener('click', function(e){
+  let target = e.target;
+  let targetRow = target.parentElement.parentElement;
+  if (target.classList.contains('readmode')) {
+    changeRead(targetRow.id);
+    let hasRead = target.classList.contains('read');
+    target.textContent = hasRead ? 'Not read' : 'Read';
+    target.className = hasRead ? 'readmode notread' : 'readmode read';
+
+    
+
+  } else if (target.classList.contains('deletebook')) {
+    deleteBook(targetRow.id);
+    targetRow.remove();
+  }
+
 })
 
 
 
 function addBook(book) {
   myLibary.push(book);
+  
 }
 
 function deleteBook(id) {
@@ -50,4 +74,35 @@ function changeRead(id) {
   })
 }
 
-console.log(myLibary);
+function viewAddBook(book) {
+  let row = document.createElement('tr');
+  row.id = book.id;
+
+  let title = document.createElement('td');
+  title.textContent = book.title;
+  row.appendChild(title);
+
+  let author = document.createElement('td');
+  author.textContent = book.author;
+  row.appendChild(author);
+
+  let pages = document.createElement('td');
+  pages.textContent = book.pages;
+  row.appendChild(pages);
+
+  let read = document.createElement('td');
+  let readMode = document.createElement('button');
+  readMode.textContent = book.read ? 'Read' : 'Not read';
+  readMode.className = book.read ? 'readmode read' : 'readmode notread';
+  read.appendChild(readMode);
+  row.appendChild(read);
+
+  let remove = document.createElement('td');
+  let remodeButton = document.createElement('button');
+  remodeButton.textContent = 'Remove Book';
+  remodeButton.className = 'deletebook';
+  remove.appendChild(remodeButton);
+  row.appendChild(remove);
+
+  bookslist.appendChild(row);
+}
